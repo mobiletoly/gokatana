90b0f9bf2bf517d448af0994c21706078f52933e
Toly Pochkin <mr.toly@gmail.com>
2025-06-16 18:04:46 +0300

    add extra http status code assertion

--- kattest/assertions.go
+++ kattest/assertions.go
@@ -24,3 +24,15 @@ func AssertStatusBadRequest(t *testing.T, err error) {
 	assert.ErrorAs(t, err, &ue)
 	assert.Equal(t, ue.StatusCode, http.StatusBadRequest)
 }
+
+func AssertStatusConflict(t *testing.T, err error) {
+	var ue *kathttpc.UnexpectedStatusCodeError
+	assert.ErrorAs(t, err, &ue)
+	assert.Equal(t, ue.StatusCode, http.StatusConflict)
+}
+
+func AssertStatusInternalServerError(t *testing.T, err error) {
+	var ue *kathttpc.UnexpectedStatusCodeError
+	assert.ErrorAs(t, err, &ue)
+	assert.Equal(t, ue.StatusCode, http.StatusInternalServerError)
+}
