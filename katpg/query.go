2bcb2ea131b7597f37e6ad981a68c40b125cf173
Toly Pochkin <mr.toly@gmail.com>
2025-08-15 17:51:11 +0300

    add ExecWithResult helper

--- katpg/query.go
+++ katpg/query.go
@@ -17,6 +17,15 @@ func GetOptionalRow[T any](ctx context.Context, tx pgx.Tx, sql string, args ...a
 	return record, nil
 }
 
+func ExecWithResult[T any](ctx context.Context, tx pgx.Tx, sql string, args ...any) (*T, error) {
+	rows, _ := tx.Query(ctx, sql, args...)
+	record, err := pgx.CollectExactlyOneRow(rows, pgx.RowToAddrOfStructByName[T])
+	if err != nil {
+		return nil, err
+	}
+	return record, nil
+}
+
 func GetRowsTx[T any](ctx context.Context, tx pgx.Tx, sql string, args ...any) ([]*T, error) {
 	rows, _ := tx.Query(ctx, sql, args...)
 	return pgx.CollectRows(rows, pgx.RowToAddrOfStructByName[T])
